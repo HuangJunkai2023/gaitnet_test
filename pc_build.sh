11e91e59db99abddadf757d145c215313364ebf6
huangjk <442827947@qq.com>
2025-12-30 15:21:53 +0800

    实时异常检测+conoda环境备份

--- pc_build.sh
+++ pc_build.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-ENVDIR=${ENVDIR:-~/pkgenv }
+set -e
 
-# If CentOS server, set C++ compiler manually
-if [ -f /etc/redhat-release ]; then
-    export CC=/opt/ohpc/pub/compiler/gcc/8.3.0/bin/gcc
-    export CXX=/opt/ohpc/pub/compiler/gcc/8.3.0/bin/g++
-fi
+CONDA_PREFIX=$HOME/miniconda3/envs/gaitnet_new
+ENVDIR=${ENVDIR:-$HOME/pkgenv}
 
 mkdir -p build
 pushd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$ENVDIR \
-      -DCMAKE_INSTALL_PREFIX=$ENVDIR \
-      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
-      -DCMAKE_INSTALL_RPATH=$ENVDIR \
-      -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-      -DFCL_INCLUDE_DIRS=$ENVDIR/include/fcl \
-      -DPYTHON_EXECUTABLE=$HOME/miniconda3/envs/gaitnet/bin/python \
-      -DPYTHON_INCLUDE_DIR=$HOME/miniconda3/envs/gaitnet/include/python3.6m \
-      -DPYTHON_LIBRARY=$HOME/miniconda3/envs/gaitnet/lib/libpython3.6m.so \
-      ..
-make -j16
+
+cmake \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH="${CONDA_PREFIX};${ENVDIR}" \
+  -DCMAKE_INSTALL_PREFIX="${ENVDIR}" \
+  -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
+  -DCMAKE_INSTALL_RPATH="${ENVDIR}" \
+  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+  -DFCL_INCLUDE_DIRS="${ENVDIR}/include/fcl" \
+  -DPYTHON_EXECUTABLE="${CONDA_PREFIX}/bin/python" \
+  -DPYTHON_INCLUDE_DIR="${CONDA_PREFIX}/include/python3.6m" \
+  -DPYTHON_LIBRARY="${CONDA_PREFIX}/lib/libpython3.6m.so" \
+  -Dpybind11_DIR="${CONDA_PREFIX}/share/cmake/pybind11" \
+  ..
+
+make -j$(nproc)
 popd
