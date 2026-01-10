1505f9f17d40de84b6b8b5fbd64fb3cb48d8c069
huangjk <442827947@qq.com>
2026-01-10 16:46:49 +0800

    change pwd

--- start_training.sh
+++ start_training.sh
@@ -5,14 +5,14 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate gaitnet
 
 # 设置 Python 路径和库路径
-export PYTHONPATH=$PYTHONPATH:/home/huaxi/scu_robotics/gaitnet_test/python
+export PYTHONPATH=$PYTHONPATH:/home/hx/code/gaitnet_test/python
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 # 设置使用GPU 0
 export CUDA_VISIBLE_DEVICES=0
 
 # 进入 python 目录（环境文件使用相对路径 ../data/）
-cd /home/huaxi/scu_robotics/gaitnet_test/python
+cd /home/hx/code/gaitnet_test/python
 
 # 运行训练脚本
 python ray_train.py --config ppo_medium_pc --config-file ray_config.py --name emg_reward_training --env ../data/env.xml "$@" #--checkpoint ./ray_results/emg_reward_training/
--- start_viewer.sh
+++ start_viewer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /home/huaxi/scu_robotics/gaitnet_test
+cd /home/hx/code/gaitnet_test
 
 # Activate conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate gaitnet
 
 # Set Python path and library path
-export PYTHONPATH=$PYTHONPATH:/home/huaxi/scu_robotics/gaitnet_test/python
+export PYTHONPATH=$PYTHONPATH:/home/hx/code/gaitnet_test/python
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 # Run viewer
